f7c33ccaaa1ed7aea7865f4ba439aa3677c1f1df
John Doe <79988159+John8790909@users.noreply.github.com>
2022-06-12 14:20:48 -0400

    ✅| first commit

--- framework/shopify/next.config.js
+++ framework/shopify/next.config.js
@@ -5,4 +5,10 @@ module.exports = {
   images: {
     domains: ['cdn.shopify.com'],
   },
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 25 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 5,
+  },
 }
